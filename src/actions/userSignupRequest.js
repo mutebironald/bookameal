42023cd4cb40424641926f2bbed01b03d13e34ec
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-04 14:46:19 +0300

    Change to axios request

--- src/actions/userSignupRequest.js
+++ src/actions/userSignupRequest.js
@@ -1,22 +1,27 @@
-export const userSignupRequest = data => dispatch => {
-    console.log("Test Action")
-    console.log(data)
-    let payload = {
-        method: "POST",
-        body: data,
-        headers: {
-            "Content-Type": "application/json",
+import axios from 'axios';
 
-        }
-    }
-    fetch(`https://bookameal-.herokuapp.com/auth/register`, payload)
-    .then(response => response.json())
-    .then(data => dispatch(
-        {
-            type: "SIGNUP_USER",
-            payload: data
-        }
-    ))
-    .catch(error=>error)
+export function userSignupRequest(data){
+    return function(dispatch){
+        console.log("Test Action")
+        console.log(data)
+        // let payload = {
+        //     method: "POST",
+        //     body: data,
+        //     headers: {
+        //         "Content-Type": "application/json",
 
+        //     }
+        return axios.post(`/auth/register`, data)
+        .then(response => {
+            console.log(response.data)
+            dispatch({type: "SIGNUP_USER",payload: data});
+            return {signedUp: true, message: "You have successfully signed up" };
+        })
+        .catch(error => {
+            console.log('errors', error);
+            return {signedUp: false, message: "wrong email or password"};
+            // console.log('register error', error.response.data)
+        })
+
+}
 }
