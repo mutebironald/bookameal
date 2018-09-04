9237ce6d9e42b77ac6fb300efa4e663bdd64821c
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-04 14:50:36 +0300

    Replace axios with fetch

--- src/actions/deleteMealRequest.js
+++ src/actions/deleteMealRequest.js
@@ -1,55 +1,17 @@
-// export const addMealRequest = data => dispatch => {
-//     console.log("Adding my meals");
-//     console.log(data);
-//     let payload = {
-//         method: 'POST',
-//         body: data,
-//         headers: {
-//             "Content-Type": "application/json",
-//             // "Authorization": this.props.loginData.access_token
-            
-//             // "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MzM2NDYzMTMsInN1YiI6MSwiaXNfYWRtaW4iOiJGYWxzZSJ9.9Y6jZwyhiKPDCO8cAFFA948TFKaGYKQrm0ZF0pFyXE8"
-
-//         }
-//     }
+import axios from 'axios'
+export function deleteMealRequest(meal_id){
+    console.log("you are deleting")
     
+    return function(dispatch){
 
-//     fetch(`http://127.0.0.1:5000/api/v1/meals`, payload)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error=>console.log(error))
-//     // console.log(fetch(`https://bookameal-.herokuapp.com/auth/signup`, payload)
-
-// }
-export const deleteMealRequest = data => dispatch => {
-    console.log("you are deleting");
-    console.log(data);
-    let payload = {
-        method: 'DELETE',
-
-    }
-
-    fetch(`http://127.0.0.1:5000/api/v1/meals/${meal.id}`, payload)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-
+    return axios.delete(`/api/v1/meals/` + meal_id)
+    .then(response => {
+        dispatch({ type: "DELETE_MEAL", meal_id});
+        return {deletedMeal: true, message: response};
+    })
+    .catch(error => {
+        console.log("error", error)
+        return {deletedMeal: false, message: "The meal specified is not present"};
+    })
 }
-
-
-// class deleteMealRequest{
-//     static deleteMeal(meal){
-//         const request = new Request(`http://127.0.0.1:5000/api/v1/meals/${meal.id}`, {
-//             method: 'DELETE'
-//         });
-
-//         return fetch(request).then(response => {
-//             return response.json();
-//         })
-//         .catch(error => {
-//             return error;
-//         });
-//     }
-// }
-
-// export default deleteMealRequest;
+}
---
+++ src/components/users/LoginModal.js
@@ -0,0 +1,24 @@
+import { hideModal } from '../../action-creators/modal';
+import Modal from '../../Modal';
+
+class LoginModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClose = this.onClose.bind(this);
+  }
+
+  onClose() {
+    this.props.hideModal();
+  }
+
+  render() {
+    return (
+      <Modal onClose={this.onClose}>
+         <div className="login">
+           <h1>Login</h1>
+             <form ...login form...
+         </div>
+      </Modal>
+    );
+  }
+}
