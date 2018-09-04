c4bd57982bb3977d5af5dae9bcd384a30f150d18
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-04 14:47:45 +0300

    Change to axios request

--- src/actions/userLoginRequest.js
+++ src/actions/userLoginRequest.js
@@ -1,22 +1,25 @@
-export const userLoginRequest = data => dispatch => {
-    console.log("Test Action")
-    console.log(data)
-    let payload = {
-        method: "POST",
-        body: data,
-        headers: {
-            "Content-Type": "application/json",
+import axios from 'axios';
 
-        }
-    }
-    fetch(`https://bookameal-.herokuapp.com/auth/login`, payload)
-    .then(response => response.json())
-    .then(data => dispatch(
-        {
-            type: "LOGIN_USER",
-            payload: data
-        }
-    ))
-    .catch(error=>error)
+export function userLoginRequest(data) {
+    return function(dispatch){
+        console.log("Test Action")
 
+        return axios.post(`/auth/login`, data)
+        .then(res => {
+
+            console.log("this is the response", res.data.access_token)
+
+            localStorage.setItem('Authorization',res.data.access_token )
+
+            console.log(res.data.message)
+            console.log("blabla", res.data.admin)
+            dispatch({ type: "LOGIN_USER", payload: data});
+            return {loggedIn: true, message: res.data.message, admin: res.data.admin};
+        })
+        .catch(error=>{
+            console.log('errors', error);
+            return {loggedIn: false, message: "Please Signup then login", admin: "False"};
+
+        })
+        }
 }
