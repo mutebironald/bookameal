ddd7ef909aa374fb12799005d24d9d00be96e8b1
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-04 14:43:13 +0300

    Add not found component

---
+++ src/components/Whoops404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Whoops404({location }){
+    return(
+        <div className="whoops-404">
+               <h1>Resource not found at'{location.pathname}'</h1>
+             </div>
+    );
+}
