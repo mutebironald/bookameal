1c50cc3c1c9deb23fc260308c3419a25de416c60
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-05 13:26:19 +0300

    Add to store

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,8 +5,16 @@ export const ADD_FLASH_MESSAGE  = 'ADD_FLASH_MESSAGE';
 export const USER_LOGIN = 'USER_LOGIN';
 
 
-export const ADD_MEAL = 'ADD_MEAL';
+// export const ADD_MEAL = 'ADD_MEAL';
 export const UPDATE_MEAL = 'UPDATE_MEAL';
 export const GET_MEALS = 'GET_MEALS';
 export const DELETE_MEAL = 'DELETE_MEAL';
 
+//
+export const LOGIN_USER = 'LOGIN_USER';
+export const SIGNUP_USER = 'SIGNUP_USER';
+export const ADD_MEAL = 'ADD_MEAL';
+
+export const LOGIN_ADMIN = 'LOGIN_ADMIN';
+export const SIGNUP_ADMIN = "SIGNUP_ADMIN";
+export const GET_ORDER = "GET_ORDER"
