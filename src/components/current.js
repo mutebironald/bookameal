8cd2a1e77a8a7069aed431240e791821dc560f9c
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-06 12:55:48 +0300

    Function to implement weekday

---
+++ src/components/current.js
@@ -0,0 +1,16 @@
+const getCurrentDay = () => {
+    const numberDay = new Date().getDay();
+    const days = [
+      { key: 1, name: "Monday" },
+      { key: 2, name: "Tuesday" },
+      { key: 3, name: "Wednesday" },
+      { key: 4, name: "Thursday" },
+      { key: 5, name: "Friday" },
+      { key: 6, name: "Saturday" },
+      { key: 7, name: "Sunday" }
+    ];
+    const today = days.find(day => day.key === numberDay);
+    return today.name;
+  };
+  export default getCurrentDay;
+  
