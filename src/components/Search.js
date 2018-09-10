df962929a7eb2cee97de539a8f4955e4449f3718
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-10 14:01:09 +0300

    Add search feature

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React from  'react';
+
+class Search extends React.Component{
+
+    handleSearch(event){
+        console.log(event.target.value)
+    }
+
+    render(){
+        return(
+            <div className="row">
+            <div className="input-field">
+                <label>Search</label>
+                <input type="text" onKeyUp={this.handleSearch.bind(this)}/>
+            </div>
+               
+            </div>
+        );
+    }
+}
+
+export default Search;
