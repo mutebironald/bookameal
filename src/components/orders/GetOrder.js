d84eddde9098e7dcbcd9cedc0a14f713f879c14c
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-10 13:53:49 +0300

    Remove unused component

--- src/components/orders/GetOrder.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import '../.././index.css';
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
-import GetOrders from './GetOrders';
-
-class GetOrders extends React.Component{
-    
-}
