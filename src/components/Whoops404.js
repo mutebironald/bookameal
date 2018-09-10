696eca4855833b13d26bd7e8cf10a14f1ad54322
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-10 13:56:10 +0300

    Format file to Es6 standard

--- src/components/Whoops404.js
+++ src/components/Whoops404.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function Whoops404({location }){
-    return(
-        <div className="whoops-404">
-               <h1>Resource not found at'{location.pathname}'</h1>
-             </div>
-    );
+export default function Whoops404({ location }) {
+  return (
+    <div className="whoops-404">
+      <h1>
+        Resource not found at'{location.pathname}'
+      </h1>
+    </div>
+  );
 }
