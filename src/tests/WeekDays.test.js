9b6357c68ce890ca7575b2cbb5c96c1c23c27889
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-10 13:49:47 +0300

    Add Weekday tests

---
+++ src/tests/WeekDays.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import WeekDays from '../components/Weekdays'
+
+describe("WeekDays component", () => {
+    let wrapper;
+    let mockFn;
+    beforeEach(() => {
+        mockFn = jest.fn();
+        wrapper = shallow(<WeekDays/>);
+    });
+    it("renders successfully", () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("should show meals on clicking", () => {
+        wrapper
+            .find("#monday")
+            .first()
+            .simulate("click")
+
+        expect(mockFn.mock.calls.length).toBe(1);
+    });
+});
