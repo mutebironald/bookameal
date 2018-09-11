ed8495aba56e7d39bb0f448cbde26ca3076db651
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-11 21:14:31 +0300

    Format to Es6

--- src/actions/addMenuRequest.js
+++ src/actions/addMenuRequest.js
@@ -3,11 +3,13 @@ import instance from '../actions/instance';
 
 
 export const addMenuRequest = (meal_id) => dispatch => {
-    instance.post(`/api/v1/menu`, meal_id,  {headers:{"Authorization": localStorage.getItem('Authorization')}})
-    .then(response => response)
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+    instance.post(`/api/v1/menu`, meal_id, {
+            headers: {
+                "Authorization": localStorage.getItem('Authorization')
+            }
+        })
+        .then(response => response)
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
 
 }
-
-
