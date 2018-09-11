0263b1d71a2c70598a707c7f9171bce4279f65b5
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-11 23:01:57 +0300

    Change to heroku url

--- src/actions/instance.js
+++ src/actions/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://bookameal-.herokuapp.com",
   headers: { Authorization: `${localStorage.getItem("authUserToken")}` }
 });
 
