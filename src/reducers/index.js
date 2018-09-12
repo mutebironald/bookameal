0c79094b346f0ce40393cdc88a1243fe25471262
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-12 20:56:49 +0300

    Organize imports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,43 +2,23 @@ import { combineReducers } from 'redux';
 import user_reducer from './user_reducer';
 import signUpReducer from './signUpReducer';
 import loginReducer from './loginReducer';
-
-
-import addMealReducer from './addMealReducer'
-import updateMealReducer from './updateMealReducer'
-import deleteMealReducer from './deleteMealReducer'
-
-
-import addMenuReducer from './addMenuReducer'
-
-
-import addOrderReducer from './addOrderReducer'
-
-
-import meals from './postReducer';
+import addMealReducer from './addMealReducer';
+import updateMealReducer from './updateMealReducer';
+import deleteMealReducer from './deleteMealReducer';
+import addMenuReducer from './addMenuReducer';
+import addOrderReducer from './addOrderReducer';
 import adminloginReducer from './adminloginReducer';
-
 
 const mainReducer = combineReducers({
     user: user_reducer,
     signupData: signUpReducer,
     authData: loginReducer,
-
     adminData: adminloginReducer,
-
-    meals: meals,
     addmeal: addMealReducer,
     updatemeal: updateMealReducer,
-   
     deletemeal: deleteMealReducer,
-
     addmenu: addMenuReducer,
-    
-
     addorder: addOrderReducer,
-    
-
-
 
 });
 
