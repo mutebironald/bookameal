123de332be284a8d7c6c32cd9ecb147f7698f5fc
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-17 18:08:00 +0300

    Fix bug in deletemeal

--- src/actions/deleteMealRequest.js
+++ src/actions/deleteMealRequest.js
@@ -6,12 +6,14 @@ export default function deleteMealRequest(meal_id){
     
     return function(dispatch){
 
-    return instance.delete(`/api/v1/meals/` + meal_id)
+    return instance.delete(`/api/v1/meals/` + meal_id, {headers:{"Authorization": localStorage.getItem('Authorization')}})
     .then(response => {
+        // console.log("response.data")
         dispatch({ type: "DELETE_MEAL", meal_id});
         return {deletedMeal: true, message: response};
     })
     .catch(error => {
+        // console.log("error.response.data")
         return {deletedMeal: false, message: "The meal specified is not present"};
     })
 }
