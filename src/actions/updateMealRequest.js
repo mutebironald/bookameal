e4b2858122cb516f2c8e50d771189ecc916b8290
mutebironald4 <mutebironaldroninho@gmail.com>
2018-09-17 18:08:21 +0300

    Add authorization to meal update

--- src/actions/updateMealRequest.js
+++ src/actions/updateMealRequest.js
@@ -4,7 +4,7 @@ import instance from '../actions/instance';
 export default function updateMealRequest(id, data){
     return function(dispatch){
 
-    return instance.put("/api/v1/meals/" + id, data)
+    return instance.put("/api/v1/meals/" + id, data, {headers:{"Authorization": localStorage.getItem('Authorization')}})
     .then(response =>{
         dispatch({ type: "UPDATE_MEAL", payload: data, id});
         return {updatedMeal: true, message: "You have successfully updated the meal"};
